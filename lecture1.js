19a9134d01a6b12588d7f597bb6f3c353efee87c
Tarun Gupta <tarungutpa854597@gmail.com>
2026-07-23 15:34:43 +0530

    lecture1

---
+++ lecture1.js
@@ -0,0 +1,44 @@
+console.log("hello world");
+var name="traun";
+console.log(name);
+console.log(typeof(name));
+var name="tej";
+console.log(name);
+let age=22;
+age=23;
+ age=45;
+console.log(age);
+const c="india";
+console.log(c);
+var bo=true;
+console.log(typeof(bo));
+var h=null;
+var g=5;
+console.log(typeof(h));
+console.log(typeof(g));
+var arr={
+    name:"tarun",
+    class:34,
+    sem:3
+};
+console.log(arr);
+console.log(typeof(arr));
+age=age*2;
+console.log(age);
+var a=10;
+var b=10;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+
+for(let i=0;i<5;i++)
+{ 
+    var ans=""
+    for(let j=0;j<i;j++)
+    {
+       ans=ans+"*";
+      
+    }
+     console.log(ans);
+}
