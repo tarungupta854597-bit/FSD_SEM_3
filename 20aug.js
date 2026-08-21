ebf79139c1e5a6d8aa4a456768e44630f2143a91
Tarun Gupta <tarungutpa854597@gmail.com>
2026-08-21 11:32:16 +0530

    leacture

--- 20aug.js
+++ 20aug.js
@@ -1,26 +1,53 @@
-const student={
-    1:'cse',
-    name:'js'
-};
-console.log(student['1']);
-// const sym=symble('btech');
-// const person=new Object();
-// person[sym]=123;
-// person.name='js';
-// console.log(person);
-let p={
-    name:'a',
-};
-p={age:25};
-p.dept='cse';
-console.log(p);
-// method to add an object
-const Person={
-    name:'abc',
+// // const student={
+// //     1:'cse',
+// //     name:'js'
+// // };
+// // console.log(student['1']);
+// // // const sym=symble('btech');
+// // // const person=new Object();
+// // // person[sym]=123;
+// // // person.name='js';
+// // // console.log(person);
+// // let p={
+// //     name:'a',
+// // };
+// // p={age:25};
+// // p.dept='cse';
+// // console.log(p);
+// // // method to add an object
+// // const Person={
+// //     name:'abc',
 
+// // };
+// // Person.greet=function()
+// // {
+// //     console.log('hello'+ this.name)
+// // }
+// // console.log(Person.greet());
+// const person={
+//     name:'abc',
+//     greet:function(){
+//         console.log('hall0'+this.name);
+//     }
+// };
+// person.greet()
+// const Person={
+//     name:'tarun',
+//     age:'21',
+//     laptop:{
+//         cpu:'i5',
+//         ram:'16',
+//         brand:'dell',
+//     }
+// }
+// console.log(Person.laptop.cpu);
+const a={
+    a:1,
+    b:2
 };
-Person.greet=function()
-{
-    console.log('hello'+ this.name)
-}
-console.log(Person.greet());
+const b={
+    b:3,
+    c:4,
+};
+let m=Object.assign({},a,b);
+console.log(m);
