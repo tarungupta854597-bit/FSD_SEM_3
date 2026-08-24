54852ab3c4cb800be8662dfc7e8913bed69e7b22
Tarun Gupta <tarungutpa854597@gmail.com>
2026-08-24 09:29:44 +0530

    24aug

---
+++ 24aug.js
@@ -0,0 +1,37 @@
+// // // let x=10;
+// // // let str=" ";
+// // // console.log(typeof str);
+// // // let str1=String('10');
+// // // console.log(str+=str1)
+
+
+// // //Multiline String
+// // let str=` this is 
+// // multiline string
+// // in javascript`;
+// // console.log(str);
+
+// // //Accessing Characters
+// // let x='JavaScript';
+// // x[0]='A';
+// // console.log(x);
+
+// // const y='javaScript'
+// // y[0]='A';
+// // console.log(y)
+
+// // //slice(start,end)
+
+// // //comparition string
+// let  str="tarun";
+// console.log(str);
+// let str1="v"+str.slice(1);
+// console.log(str1);
+// console.log(str);
+
+const str1='hello';
+const str=new String('hello')
+console.log(str1==str);
+console.log(str===str1);
+console.log(typeof(str1));
+console.log(typeof(str));
